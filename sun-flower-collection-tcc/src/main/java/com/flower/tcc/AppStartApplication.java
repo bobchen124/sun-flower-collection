c19e609e6b2c1b42ca63a6603846a967c725fe56
陈波 <chen.bob124@gmail.com>
2019-08-25 16:42:38 +0800

    add tcc

---
+++ sun-flower-collection-tcc/src/main/java/com/flower/tcc/AppStartApplication.java
@@ -0,0 +1,18 @@
+package com.flower.tcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Desc:
+ * @Author: chenbo
+ * @Date: 2019/8/25 16:33
+ **/
+@SpringBootApplication
+public class AppStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppStartApplication.class, args);
+    }
+
+}
